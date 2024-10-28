f9d5bc0816513bcd507e1a03e6dbffb2fdec8966
AtlasVIA <atlas@vialabs.io>
2024-10-28 06:46:29 +0100

    dont push dist artifacts

--- dist/chainConfigs.d.ts
+++
@@ -1,3 +0,0 @@
-import { ChainConfigs } from './types';
-export declare const mainnetConfigs: ChainConfigs;
-export declare const testnetConfigs: ChainConfigs;
--- dist/global.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const globalConfig: {
-    accountant: {
-        mainnet: string;
-        testnet: string;
-    };
-};
-export declare function getAccountantAddress(isTestnet: boolean): string;
