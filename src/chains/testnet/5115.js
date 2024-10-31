a14bcf2e70800b8f07fb67f13a62432188371f63
AtlasVIA <atlas@vialabs.io>
2024-10-31 09:58:38 +0100

    added citrea-testnet config

---
+++ src/chains/testnet/5115.js
@@ -0,0 +1,11 @@
+const chainConfig = {
+  "name": "citrea-testnet",
+  "network": "testnet",
+  "type": "evm",
+  "chainId": 5115,
+  "message": "0x566B40Dd59A868c244E1353368e08ddaD1C1d74f",
+  "feeToken": "0x0000000000000000000000000000000000000000",
+  "weth": "0xc2e7eA22A69Fff2ac96EB9b306A562C9fb299b16",
+};
+
+module.exports = chainConfig;
