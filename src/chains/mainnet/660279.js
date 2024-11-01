8365e877400558cd77f22778b8e568be000a17e2
AtlasVIA <atlas@vialabs.io>
2024-11-01 01:48:59 +0100

    updated xai weth token

--- src/chains/mainnet/660279.js
+++ src/chains/mainnet/660279.js
@@ -5,7 +5,7 @@ const chainConfig = {
   "chainId": 660279,
   "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
   "feeToken": "0x1E3769Bd5fB2e9e9e7D4ED8667c947661F9A82E3",
-  "weth": "0xbee82cfdaff4a6aa4e4793cb81eb1c2e79ac463c"
+  "weth": "0x3fB787101DC6Be47cfe18aeEe15404dcC842e6AF"
 };
 
 module.exports = chainConfig;
