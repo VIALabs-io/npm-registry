479411a3cbece4b797a379db700d56f911af9c0d
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2024-11-02 14:54:24 +0100

    Update 5115.js

--- src/chains/testnet/5115.js
+++ src/chains/testnet/5115.js
@@ -4,7 +4,7 @@ const chainConfig = {
   "type": "evm",
   "chainId": 5115,
   "message": "0x566B40Dd59A868c244E1353368e08ddaD1C1d74f",
-  "feeToken": "0x0000000000000000000000000000000000000000",
+  "feeToken": "0x28d03e97e68Fb138a2318A2627806dfAF9972755",
   "weth": "0xc2e7eA22A69Fff2ac96EB9b306A562C9fb299b16",
 };
 
