ffe2beb51af32fa89cd946a41b5b36a9ca6d8da0
AtlasVIA <atlas@vialabs.io>
2024-11-02 04:09:12 +0100

    added usdc to optimism

--- src/chains/mainnet/10.js
+++ src/chains/mainnet/10.js
@@ -6,6 +6,7 @@ const chainConfig = {
   "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
   "feeToken": "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85",
   "weth": "0x4200000000000000000000000000000000000006",
+  "usdc": "0x0b2c639c533813f4aa9d7837caf62653d097ff85",
   "circleTokenMessenger": "0x2B4069517957735bE00ceE0fadAE88a26365528f",
   "circleMessageTransmitter": "0x4d41f22c5a0e5c74090899e5a8fb597a8842b3e8",
   "featureGateway": "0x0000000000000000000000000000000000000000", // Placeholder address
