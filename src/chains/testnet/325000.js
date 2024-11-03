4410165d4b8ab68c435ec3a6c987d8bcd7d94f31
AtlasVIA <atlas@vialabs.io>
2024-11-03 08:51:05 +0100

    added pathway address to camp testnet

--- src/chains/testnet/325000.js
+++ src/chains/testnet/325000.js
@@ -6,6 +6,7 @@ const chainConfig = {
   "message": "0x3B5b764229b2EdE0162220aF51ab6bf7f8527a4F",
   "feeToken": "0x2777023E33B88CE622B15eCfA4ADa3cb5a9C545f",
   "weth": "0x0EFafca24E5BbC1C01587B659226B9d600fd671f",
+  "usdc": "0x2777023E33B88CE622B15eCfA4ADa3cb5a9C545f",
   "protoCCTPGateway": "0x05ec9b2A78197ecdf5295453e6040b739809Bbd5",
 };
 
