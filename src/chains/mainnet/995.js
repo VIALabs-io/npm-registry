67bada09922c1a240d65c00f03759649d1005554
AtlasVIA <atlas@vialabs.io>
2024-11-03 08:19:40 +0100

    added usdc pathways

--- src/chains/mainnet/995.js
+++ src/chains/mainnet/995.js
@@ -6,6 +6,7 @@ const chainConfig = {
   "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
   "feeToken": "",
   "weth": "0xCFaEB74409E4C6756C43F75455fc42A6A3FdEb1f",
+  "usdc": "0x85DAc390d61cdaCb346701cC4948870A704d3f4a",
   "protoCCTPGateway": "0xd6d9317E95Ca1cEDe3D06F3D6844a739194212cc",
 };
 
