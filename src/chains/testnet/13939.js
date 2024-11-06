bcd62a6710e9b64c67fe7b330fa5d2fb578620d5
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2024-11-06 19:37:34 +0100

    Create 13939.js

---
+++ src/chains/testnet/13939.js
@@ -0,0 +1,11 @@
+const chainConfig = {
+  "name": "reef-testnet",
+  "network": "testnet",
+  "type": "reef",
+  "chainId": 13939,
+  "message": "0x0000000000000000000000000000000000000000",
+  "feeToken": "0x0000000000000000000000000000000000000000",
+  "weth": "0x0000000000000000000000000000000000000000"
+};
+
+module.exports = chainConfig;
