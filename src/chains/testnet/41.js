1d2f01da4f39ca5349e48a2d76f7d696e8dd768a
AtlasVIA <atlas@vialabs.io>
2024-11-08 02:02:43 +0100

    added citrea and telos testnet protocctp gateway deployment addresses

--- src/chains/testnet/41.js
+++ src/chains/testnet/41.js
@@ -5,7 +5,8 @@ const chainConfig = {
   "chainId": 41,
   "message": "0xF8d80d6E52b5B8484a7CD27a5C0F3D35695c57dF",
   "feeToken": "0x2777023E33B88CE622B15eCfA4ADa3cb5a9C545f",
-  "weth": "0x02dA7eeEB722A126CCC53085d489ca0e1BAbc55E"
+  "weth": "0x02dA7eeEB722A126CCC53085d489ca0e1BAbc55E",
+  "protoCCTPGateway": "0xD0a91771452973789eC279BC21E03fef4F7c97C1",
 };
 
 module.exports = chainConfig;
--- src/chains/testnet/5115.js
+++ src/chains/testnet/5115.js
@@ -6,6 +6,7 @@ const chainConfig = {
   "message": "0x566B40Dd59A868c244E1353368e08ddaD1C1d74f",
   "feeToken": "0x28d03e97e68Fb138a2318A2627806dfAF9972755",
   "weth": "0xc2e7eA22A69Fff2ac96EB9b306A562C9fb299b16",
+  "protoCCTPGateway": "0x289992562cba9EB7447C0579d626ABb7960802cb",
 };
 
 module.exports = chainConfig;
