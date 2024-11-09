47d43eb05572df86757160f2765ff87ecd5147d9
AtlasVIA <atlas@vialabs.io>
2024-11-09 07:59:16 +0100

    add pathway usdc address to citrea

--- src/chains/testnet/5115.js
+++ src/chains/testnet/5115.js
@@ -5,6 +5,7 @@ const chainConfig = {
   "chainId": 5115,
   "message": "0x566B40Dd59A868c244E1353368e08ddaD1C1d74f",
   "feeToken": "0x28d03e97e68Fb138a2318A2627806dfAF9972755",
+  "usdc": "0x28d03e97e68Fb138a2318A2627806dfAF9972755",
   "weth": "0xc2e7eA22A69Fff2ac96EB9b306A562C9fb299b16",
   "protoCCTPGateway": "0x289992562cba9EB7447C0579d626ABb7960802cb",
 };
