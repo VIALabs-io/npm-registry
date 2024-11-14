eb78970aff30dcdf3dca42ca5405c38ed7a17cbe
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2024-11-14 19:32:26 +0100

    Update 13939.js

--- src/chains/testnet/13939.js
+++ src/chains/testnet/13939.js
@@ -3,9 +3,9 @@ const chainConfig = {
   "network": "testnet",
   "type": "reef",
   "chainId": 13939,
-  "message": "0x0000000000000000000000000000000000000000",
-  "feeToken": "0x0000000000000000000000000000000000000000",
-  "weth": "0x0000000000000000000000000000000000000000"
+  "message": "0xf7af4C3ecA8Dd2E2a59eD44060537a09f701C6ed",
+  "feeToken": "0x72761eBBA6395D6deE4A8e273c1E487a72693c46",
+  "weth": "0x0000000000000000000000000000000001000000" // @note unwrap weth MUST BE FALSE!
 };
 
 module.exports = chainConfig;
