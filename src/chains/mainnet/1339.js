dd89d737c3fb824e05b179cbd49d176411b8db65
AtlasVIA <atlas@vialabs.io>
2024-12-11 07:22:24 +0100

    elysium mainnet

---
+++ src/chains/mainnet/1339.js
@@ -0,0 +1,11 @@
+const chainConfig = {
+  "name": "Elysium Mainnet",
+  "network": "mainnet",
+  "type": "evm",
+  "chainId": 1339,
+  "message": "",
+  "feeToken": "0x0000000000000000000000000000000000000000",
+  "weth": "0xd80ef77b0289732e13d1769850b5a70ecc196777"
+};
+
+module.exports = chainConfig;
--- src/chains/mainnet/1689.js
+++ src/chains/mainnet/1689.js
@@ -5,7 +5,7 @@ const chainConfig = {
   "chainId": 1689,
   "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
   "feeToken": "0x0000000000000000000000000000000000000000",
-  "weth": "0xd80ef77b0289732e13d1769850b5a70ecc196777"
+  "weth": "0x0000000000000000000000000000000000000000"
 };
 
 module.exports = chainConfig;
