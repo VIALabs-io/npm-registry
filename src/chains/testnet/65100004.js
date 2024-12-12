8b6773d3d93fed4d9179299d1aa9ef133bb45b49
AtlasVIA <atlas@vialabs.io>
2024-12-12 07:08:44 +0100

    autonity 3 testnet

---
+++ src/chains/testnet/65100004.js
@@ -0,0 +1,11 @@
+const chainConfig = {
+  "name": "autonity-testnet",
+  "network": "testnet",
+  "type": "evm",
+  "chainId": 65100004,
+  "message": "0x0EFafca24E5BbC1C01587B659226B9d600fd671f",
+  "feeToken": "0xc2e7eA22A69Fff2ac96EB9b306A562C9fb299b16",
+  "weth": "0x78CC50562941f385c3d0c5204385c3CA686d85b8"
+};
+
+module.exports = chainConfig;
