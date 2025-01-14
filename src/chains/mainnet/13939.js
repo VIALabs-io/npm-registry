b5e2e5992409c0ac3895057a88df058630a808e0
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-01-14 17:58:56 +0100

    Update 13939.js

--- src/chains/mainnet/13939.js
+++ src/chains/mainnet/13939.js
@@ -3,7 +3,7 @@ const chainConfig = {
   "network": "mainnet",
   "type": "reef",
   "chainId": 13939,
-  "message": "0xf7af4C3ecA8Dd2E2a59eD44060537a09f701C6ed",
+  "message": "0xdAFcF1e328c09cbc480A09f751c170CfCca3e083",
   "messageSubstrate": "5EMjsd1648TVgor7qD19pXda8mbfbDZ3NyTRUiWKZBRmPZ7e",
   "feeToken": "0x0000000000000000000000000000000001000000", // @note proper fee token doesn't exist yet
   "weth": "0x0000000000000000000000000000000001000000" // @note unwrap weth MUST BE FALSE!
