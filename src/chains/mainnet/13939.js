fb1cf75f5b8f546eafaa48792967b4bfd3bae0a6
Druuu <121195249+DruuuCLT@users.noreply.github.com>
2025-01-22 18:01:33 +0100

    Update 13939.js

--- src/chains/mainnet/13939.js
+++ src/chains/mainnet/13939.js
@@ -5,7 +5,7 @@ const chainConfig = {
   "chainId": 13939,
   "message": "0xdAFcF1e328c09cbc480A09f751c170CfCca3e083",
   "messageSubstrate": "5EMjsd1648TVgor7qD19pXda8mbfbDZ3NyTRUiWKZBRmPZ7e",
-  "feeToken": "0x0000000000000000000000000000000001000000", // @note proper fee token doesn't exist yet
+  "feeToken": "0xCA2917E6625E158be8066e8D8a584B022AB6D8D6",
   "weth": "0x0000000000000000000000000000000001000000" // @note unwrap weth MUST BE FALSE!
 };
 
