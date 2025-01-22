9b4732e252267c2f01bf505586247a45b3e59df4
Druuu <121195249+DruuuCLT@users.noreply.github.com>
2025-01-22 18:19:10 +0100

    Update 13939.js

--- src/chains/mainnet/13939.js
+++ src/chains/mainnet/13939.js
@@ -6,6 +6,7 @@ const chainConfig = {
   "message": "0xdAFcF1e328c09cbc480A09f751c170CfCca3e083",
   "messageSubstrate": "5EMjsd1648TVgor7qD19pXda8mbfbDZ3NyTRUiWKZBRmPZ7e",
   "feeToken": "0xCA2917E6625E158be8066e8D8a584B022AB6D8D6",
+  "usdc": "0xCA2917E6625E158be8066e8D8a584B022AB6D8D6",
   "weth": "0x0000000000000000000000000000000001000000" // @note unwrap weth MUST BE FALSE!
 };
 
