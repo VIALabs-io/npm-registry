a03f4d2a660efed5be62b52a79a047ca6bf85180
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-02-04 18:38:31 +0100

    Create 173750.js

---
+++ src/chains/testnet/173750.js
@@ -0,0 +1,13 @@
+const chainConfig = {
+  "name": "avalanche-echo-testnet",
+  "network": "testnet",
+  "type": "evm",
+  "chainId": 173750,
+  "message": "", // @note Not deployed by design
+  "feeToken": "0x2777023E33B88CE622B15eCfA4ADa3cb5a9C545f",
+  "usdc": "0x2777023E33B88CE622B15eCfA4ADa3cb5a9C545f",
+  "weth": "0x8228407F8bc38790997616B18AF8773016270558", // @note Temporary mock weth
+  "protoCCTPGateway": "0x135b709b4C646C203bcB669578C5F9345b9882fE",
+};
+
+module.exports = chainConfig;
