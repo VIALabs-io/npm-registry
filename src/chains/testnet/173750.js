6bcd80a1dfc643dbac21f53c81a87f54e87242a1
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-02-04 18:56:17 +0100

    Update 173750.js

--- src/chains/testnet/173750.js
+++ src/chains/testnet/173750.js
@@ -7,7 +7,7 @@ const chainConfig = {
   "feeToken": "0x2777023E33B88CE622B15eCfA4ADa3cb5a9C545f",
   "usdc": "0x2777023E33B88CE622B15eCfA4ADa3cb5a9C545f",
   "weth": "0x8228407F8bc38790997616B18AF8773016270558", // @note Temporary mock weth
-  "protoCCTPGateway": "",
+  "protoCCTPGateway": "0x3C615A128acb9E3247d4CF24CaA0a987d3816754",
 };
 
 module.exports = chainConfig;
