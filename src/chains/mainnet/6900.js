2b5c878567415f873769b7974279ecd6682f0f6c
AtlasVIA <atlas@vialabs.io>
2025-02-12 19:03:17 +0100

    nibiru mainnet

---
+++ src/chains/mainnet/6900.js
@@ -0,0 +1,11 @@
+const chainConfig = {
+  "name": "Nibiru Mainnet",
+  "network": "mainnet",
+  "type": "evm",
+  "chainId": 6900,
+  "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
+  "feeToken": "0x0000000000000000000000000000000000000000",
+  "weth": "0x7D4B7B8CA7E1a24928Bb96D59249c7a5bd1DfBe6"
+};
+
+module.exports = chainConfig;
