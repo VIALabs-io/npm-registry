a15d9c3c65c4690870e508dcc8c694c3a4202757
AtlasVIA <atlas@vialabs.io>
2025-02-12 18:27:12 +0100

    nibiru testnet

---
+++ src/chains/testnet/6911.js
@@ -0,0 +1,11 @@
+const chainConfig = {
+  "name": "nibiru-testnet",
+  "network": "testnet",
+  "type": "evm",
+  "chainId": 6911,
+  "message": "0x0eefCF172F7e5C04A8d565f4e955968221fDb18f",
+  "feeToken": "0xefbdB4f11b61AEb417AAC34504E9535f5218A8f2",
+  "weth": "0xF8d80d6E52b5B8484a7CD27a5C0F3D35695c57dF"
+};
+
+module.exports = chainConfig;
