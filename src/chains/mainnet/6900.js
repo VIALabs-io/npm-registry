47ec517db6fbedfc6555c95bced3854b964a7723
Druuu <121195249+DruuuCLT@users.noreply.github.com>
2025-02-27 18:14:52 +0100

    Update 6900.js

--- src/chains/mainnet/6900.js
+++ src/chains/mainnet/6900.js
@@ -5,7 +5,8 @@ const chainConfig = {
   "chainId": 6900,
   "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
   "feeToken": "0x0000000000000000000000000000000000000000",
-  "weth": "0x7D4B7B8CA7E1a24928Bb96D59249c7a5bd1DfBe6"
+  // "weth": "0x7D4B7B8CA7E1a24928Bb96D59249c7a5bd1DfBe6",
+  "weth": "0x1429B38e58b97de646ACd65fdb8a4502c2131484"
 };
 
 module.exports = chainConfig;
