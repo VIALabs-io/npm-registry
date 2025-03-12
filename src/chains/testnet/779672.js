c668dc0e0cb30f134873461fa1d4a6404408dca9
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-03-12 18:33:48 +0100

    Avax ICM dispatch chain

--- src/chains/testnet/779672.js
+++ src/chains/testnet/779672.js
@@ -4,10 +4,10 @@ const chainConfig = {
   "type": "evm",
   "chainId": 779672,
   "message": "0x0000000000000000000000000000000000000000", // @note Not deployed by design
-  "feeToken": "0xb81A799a812CeC90796F4531a7D694284667f7e5",
-  "usdc": "0xb81A799a812CeC90796F4531a7D694284667f7e5",
+  "feeToken": "0x0e80E22b69C48CcDC54B4D0A24D497d8203B7b1E",
+  "usdc": "0x0e80E22b69C48CcDC54B4D0A24D497d8203B7b1E",
   "weth": "0xA68E53100d11611F11A8956818F9Bd3DD3A2eE8d", // @note Temporary mock weth
-  "protoCCTPGateway": "0xC2bd850A20c02d93942AEe28235946F1542dbd2b",
+  "protoCCTPGateway": "0x144c21008a6e31291aAADec4CAc09c73bc640Fb4",
 };
 
 module.exports = chainConfig;
