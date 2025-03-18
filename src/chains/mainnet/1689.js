103b886c5ee849da2db39d7206d68c6857df48da
Druuu <121195249+DruuuCLT@users.noreply.github.com>
2025-03-18 18:40:35 +0100

    Update 1689.js

--- src/chains/mainnet/1689.js
+++ src/chains/mainnet/1689.js
@@ -5,7 +5,7 @@ const chainConfig = {
   "chainId": 1689,
   "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
   "feeToken": "0x0000000000000000000000000000000000000000",
-  "weth": "0x0000000000000000000000000000000000000000"
+  "weth": "0xd540c9cfa1aecb92c649d14d46715b7974b3ac7f"
 };
 
 module.exports = chainConfig;
