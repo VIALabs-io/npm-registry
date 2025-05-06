85439c494296c26884b4056a7b0c0f648c8d219b
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-05-06 19:31:12 +0200

    Update 841.js

--- src/chains/mainnet/841.js
+++ src/chains/mainnet/841.js
@@ -4,8 +4,10 @@ const chainConfig = {
   "type": "evm",
   "chainId": 841,
   "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
-  "feeToken": "",
-  "weth": "0x5d0Fa4C5668E5809c83c95A7CeF3a9dd7C68d4fE"
+  "feeToken": "0x8712796136Ac8e0EEeC123251ef93702f265aa80",
+  "usdc": "0x8712796136Ac8e0EEeC123251ef93702f265aa80",
+  "weth": "0x5d0Fa4C5668E5809c83c95A7CeF3a9dd7C68d4fE",
+  "protoCCTPGateway": "0xdD7f4dE4F625D51AE39541d1AC67967878A4A98c",
 };
 
 module.exports = chainConfig;
