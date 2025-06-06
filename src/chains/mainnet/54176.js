02c2b55d9878820afef884c81d42f0f6af609e72
AtlasVIA <atlas@vialabs.io>
2025-06-06 19:14:05 +0200

    overprotocol

---
+++ src/chains/mainnet/54176.js
@@ -0,0 +1,11 @@
+const chainConfig = {
+  "name": "OverProtocol Mainnet",
+  "network": "mainnet",
+  "type": "evm",
+  "chainId": 54176,
+  "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
+  "feeToken": "0x0000000000000000000000000000000000000000",
+  "weth": "0xd6e9E39c54e2340b498140843E4D7a0a240DE43f",
+};
+
+module.exports = chainConfig;
