c9156d10b6c551ac2a54aa991e91e0962dc80a42
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-06-10 18:43:07 +0200

    Update 54176.js

--- src/chains/mainnet/54176.js
+++ src/chains/mainnet/54176.js
@@ -5,7 +5,7 @@ const chainConfig = {
   "chainId": 54176,
   "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
   "feeToken": "0x0000000000000000000000000000000000000000",
-  "weth": "0xd6e9E39c54e2340b498140843E4D7a0a240DE43f",
+  "weth": "0x59c914C8ac6F212bb655737CC80d9Abc79A1e273",
 };
 
 module.exports = chainConfig;
