74a069473206ccd538e9bc744005ed4fd141f80a
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-07-02 19:50:08 +0200

    Create 5920.js

---
+++ src/chains/testnet/5920.js
@@ -0,0 +1,11 @@
+const chainConfig = {
+  "name": "kadena-testnet",
+  "network": "testnet",
+  "type": "evm",
+  "chainId": 5920,
+  "message": "0x861b0FC81737122C5BeC247dF709Ca1165cC7409",
+  "feeToken": "0x2777023E33B88CE622B15eCfA4ADa3cb5a9C545f",
+  "weth": "0x40df78EF054B968477Bc09060FcB15A0f350c379"
+};
+
+module.exports = chainConfig;
