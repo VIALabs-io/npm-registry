2adad0713e10f98c0b7c11dd89ee30af612be35c
Druuu <121195249+DruuuCLT@users.noreply.github.com>
2025-07-07 18:50:01 +0200

    Update 779672.js

--- src/chains/testnet/779672.js
+++ src/chains/testnet/779672.js
@@ -4,10 +4,10 @@ const chainConfig = {
   "type": "evm",
   "chainId": 779672,
   "message": "0x0000000000000000000000000000000000000000", // @note Not deployed by design
-  "feeToken": "0x0e80E22b69C48CcDC54B4D0A24D497d8203B7b1E",
-  "usdc": "0x0e80E22b69C48CcDC54B4D0A24D497d8203B7b1E",
-  "weth": "0xA68E53100d11611F11A8956818F9Bd3DD3A2eE8d", // @note Temporary mock weth
-  "protoCCTPGateway": "0x144c21008a6e31291aAADec4CAc09c73bc640Fb4",
+  "feeToken": "0xA1AeC40c1B58226407B8b0dF050c8D9ab0df7a7D",
+  "usdc": "0xA1AeC40c1B58226407B8b0dF050c8D9ab0df7a7D",
+  "weth": "0x0B5D4d5c8FfF14322d598BF45795ce6133eF5992", // @note Temporary mock weth
+  "protoCCTPGateway": "",
 };
 
 module.exports = chainConfig;
