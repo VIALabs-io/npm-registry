b9d648203a834aaf92cde01579d5eaaaa6a62203
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-07-14 19:32:15 +0200

    B2 chains

---
+++ src/chains/mainnet/223.js
@@ -0,0 +1,12 @@
+const chainConfig = {
+  "name": "B2 Mainnet",
+  "network": "mainnet",
+  "type": "evm",
+  "chainId": 223,
+  "message": "",
+  "feeToken": "0xE544e8a38aDD9B1ABF21922090445Ba93f74B9E5", // @note Some USDC they list. They also list USDT.
+  "weth": "0x4200000000000000000000000000000000000006",
+  "explorer": "https://mainnet-blockscout.bsquared.network/",
+};
+
+module.exports = chainConfig;
---
+++ src/chains/testnet/1123.js
@@ -0,0 +1,11 @@
+const chainConfig = {
+  "name": "b2-testnet",
+  "network": "testnet",
+  "type": "evm",
+  "chainId": 1123,
+  "message": "0x84126A2D8dF65Bb32E6952c351f877E59e02a17f",
+  "feeToken": "0x448811d70486296d4e2B60E4258328957C94EaA7",
+  "weth": "0x40df78EF054B968477Bc09060FcB15A0f350c379"
+};
+
+module.exports = chainConfig;
