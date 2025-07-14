5f0d4161e10b5182300cf541667538967a28fe45
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-07-14 20:45:33 +0200

    Update 223.js

--- src/chains/mainnet/223.js
+++ src/chains/mainnet/223.js
@@ -3,7 +3,7 @@ const chainConfig = {
   "network": "mainnet",
   "type": "evm",
   "chainId": 223,
-  "message": "",
+  "message": "0x2d7E4F08FF352d2FD79466bE36b6401c4463CfA9",
   "feeToken": "0xE544e8a38aDD9B1ABF21922090445Ba93f74B9E5", // @note Some USDC they list. They also list USDT.
   "weth": "0x4200000000000000000000000000000000000006",
   "explorer": "https://mainnet-blockscout.bsquared.network/",
