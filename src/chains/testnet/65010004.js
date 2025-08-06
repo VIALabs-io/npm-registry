fd81070d543ca6fbe0d6b6aa63f0a14d5f70e95f
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-08-06 19:00:00 +0200

    Autonity Bakerloo Testnet

---
+++ src/chains/testnet/65010004.js
@@ -0,0 +1,11 @@
+const chainConfig = {
+  "name": "autonity-testnet2",
+  "network": "autonity-testnet2",
+  "type": "evm",
+  "chainId": 65010004,
+  "message": "0x84126A2D8dF65Bb32E6952c351f877E59e02a17f",
+  "feeToken": "0x448811d70486296d4e2B60E4258328957C94EaA7",
+  "weth": "0x7152e69E173D631ee7B8df89b98fd25decb7263D"
+};
+
+module.exports = chainConfig;
