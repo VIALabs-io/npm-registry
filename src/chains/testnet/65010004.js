9f23ef1367c47aad1fe3b02c99443791dcb909a2
Druuu <121195249+DruuuCLT@users.noreply.github.com>
2025-08-08 17:05:39 +0200

    Update 65010004.js

--- src/chains/testnet/65010004.js
+++ src/chains/testnet/65010004.js
@@ -6,7 +6,8 @@ const chainConfig = {
   "message": "0x84126A2D8dF65Bb32E6952c351f877E59e02a17f",
   "feeToken": "0xb3093D918131C7A474c8Fad8Fe9ac246f6E113CA",
   "usdc": "0xb3093D918131C7A474c8Fad8Fe9ac246f6E113CA",
-  "weth": "0x7152e69E173D631ee7B8df89b98fd25decb7263D"
+  "weth": "0x7152e69E173D631ee7B8df89b98fd25decb7263D",
+  "protoCCTPGateway": "0xA1AeC40c1B58226407B8b0dF050c8D9ab0df7a7D",
 };
 
 module.exports = chainConfig;
