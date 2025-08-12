374c449408d41d57de7bef90400117194659283d
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-08-12 17:46:04 +0200

    Update 65000000.js

--- src/chains/mainnet/65000000.js
+++ src/chains/mainnet/65000000.js
@@ -4,8 +4,8 @@ const chainConfig = {
   "type": "evm",
   "chainId": 65000000,
   "message": "0x2d7E4F08FF352d2FD79466bE36b6401c4463CfA9",
-  "feeToken": "",
-  "usdc": "",
+  "feeToken": "0x0000000000000000000000000000000000000000",
+  "usdc": "0x0000000000000000000000000000000000000000",
   "protoCCTPGateway": "",
   "weth": "0x08178Ac13A98eE9DeA065DE1Ef233ce0b15a72Eb"
 };
