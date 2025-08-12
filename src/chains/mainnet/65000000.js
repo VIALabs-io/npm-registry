83ac5bb66f05b13815deebf3602780dc8d910f53
DruuuCLT <121195249+DruuuCLT@users.noreply.github.com>
2025-08-12 17:39:08 +0200

    Autonity mainnet

---
+++ src/chains/mainnet/65000000.js
@@ -0,0 +1,13 @@
+const chainConfig = {
+  "name": "Autonity Mainnet",
+  "network": "mainnet",
+  "type": "evm",
+  "chainId": 65000000,
+  "message": "0x2d7E4F08FF352d2FD79466bE36b6401c4463CfA9",
+  "feeToken": "",
+  "usdc": "",
+  "protoCCTPGateway": "",
+  "weth": "0x08178Ac13A98eE9DeA065DE1Ef233ce0b15a72Eb"
+};
+
+module.exports = chainConfig;
