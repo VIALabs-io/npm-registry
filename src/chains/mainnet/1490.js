7c4435d8c7008c6a4779f3801cb1c79a79a0838f
Druuu <121195249+DruuuCLT@users.noreply.github.com>
2025-09-17 19:37:11 +0200

    Update 1490.js

--- src/chains/mainnet/1490.js
+++ src/chains/mainnet/1490.js
@@ -6,7 +6,7 @@ const chainConfig = {
   "message": "0x15AC559DA4951c796DB6620fAb286B96840D039A",
   "feeToken": "0xbCfB3FCa16b12C7756CD6C24f1cC0AC0E38569CF",
   "usdc": "0xbCfB3FCa16b12C7756CD6C24f1cC0AC0E38569CF",
-  "weth": "0xC0C0A38067Ba977676AB4aFD9834dB030901bE2d",
+  "weth": "0x3ccc3F22462cAe34766820894D04a40381201ef9", // @note no longer has time locks or whitelists
   "rpc": "https://1490.rpc.vialabs.io/",
   "explorer": "https://explorer.vitruveo.xyz/",
   "protoCCTPGateway": "0x3CBf846eD83dF3417c2dB897b3DDF7C96E7465a3",
